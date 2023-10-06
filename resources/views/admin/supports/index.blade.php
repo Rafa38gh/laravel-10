fdb3130d3782ba20a17bc015ae39482dfe7fc95e
Rafa38gh <siscaro.rafael96@gmail.com>
2023-10-05 21:52:38 -0300

    support db update

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,27 @@
 <h1>Listagem dos suportes</h1>
+
+<!-- Ao linkar uma nova página, chamamos a rota pelo seu nome -->
+<a href="{{ route('supports.create') }}">Nova Dúvida</a>
+
+<table>
+    <thead>
+        <th>assunto</th>
+        <th>status</th>
+        <th>descrição</th>
+        <th></th>
+    </thead>
+
+    <tbody>
+        @foreach ($supports as $support)
+        <tr>
+            <td>{{ $support->subject }}</td>
+            <td>{{ $support->status }}</td>
+            <td>{{ $support->body }}</td>
+            <td>
+                >
+            </td>
+        </tr>
+    
+        @endforeach
+    </tbody>
+</table>
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* Support Routs */
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::post('/supports/create', [SupportController::class, 'store'])->name('supports.store');
 
+/* Contact Routs */
 Route::get('/contato', [SiteController::class, 'contact']);
 
 Route::get('/', function () 
